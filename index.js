a60826cf7950780eae8ff3c39ce007f7b66261b2
Ameya Shrivastava <ameyshri051@gmail.com>
2023-01-07 01:54:59 +0530

    integrating socket.io

--- index.js
+++ index.js
@@ -28,4 +28,38 @@ app.use(errorHandler);
 port = process.env.PORT || 5000;
 
 //server starts listening
-app.listen(port, console.log(`server listening on http://localhost:${port}`));
+const server = app.listen(
+  port,
+  console.log(`server listening on http://localhost:${port}`)
+);
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log(`user joined room ${room}`);
+  });
+
+  socket.on("new message", (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
+    if (!chat.users) console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id === newMessageReceived.sender._id) return;
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+  });
+});

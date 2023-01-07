b1bd54794074a5d8adbb206edf12771d32af994f
Ameya Shrivastava <ameyshri051@gmail.com>
2023-01-07 17:52:14 +0530

    added sockets

--- index.js
+++ index.js
@@ -53,6 +53,9 @@ io.on("connection", (socket) => {
     console.log(`user joined room ${room}`);
   });
 
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
   socket.on("new message", (newMessageReceived) => {
     var chat = newMessageReceived.chat;
     if (!chat.users) console.log("chat.users not defined");
@@ -61,5 +64,10 @@ io.on("connection", (socket) => {
       if (user._id === newMessageReceived.sender._id) return;
       socket.in(user._id).emit("message received", newMessageReceived);
     });
+  });
+
+  socket.off("setup", () => {
+    console.log("user disconnected");
+    socket.leave(userData._id);
   });
 });
